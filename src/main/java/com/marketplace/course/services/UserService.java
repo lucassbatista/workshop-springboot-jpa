937d3e33651f9289e0aabe6d03effb4a8460f2dd
Lucas Batista <lucasq04@gmail.com>
2023-01-26 22:19:53 -0300

    Exception handling - update

--- src/main/java/com/marketplace/course/services/UserService.java
+++ src/main/java/com/marketplace/course/services/UserService.java
@@ -13,41 +13,45 @@ import com.marketplace.course.repositories.UserRepository;
 import com.marketplace.course.services.exceptions.DatabaseException;
 import com.marketplace.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
-	public User findById(long id){
-		Optional<User> obj =  userRepository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+
+	public User findById(long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			userRepository.deleteById(id);
-			
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		} catch(DataIntegrityViolationException e){
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
